fc516d372b4a19a53048a8b7a440e4d17a2afeea
shakambhar pujari <shakambhar6562@gmail.com>
2026-04-05 17:38:52 +0530

    added the usePromiseHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
-import './App.css';
-import { defaultClient } from './MockClient/MockClient';
-import { withRetry } from './utility/withRetry';
+import { useEffect } from "react";
+import "./App.css";
+import { usePromise } from "./hooks/usePromise";
+import axios from "axios";
 
 function App() {
-  const callApi = useCallback(
-    withRetry({
-      callBackFn: (signal, ...args) => {
-        return defaultClient.GET({
-          mockEndpoint: defaultClient.availableEnpoints.productList,
-          sleepTimer: 2000,
-          signal,
-        });
+  const { callApi, cancelRequest, currentState, updateState } = usePromise({
+    abortReasonDefault: "this request was cancelled",
+    dataMapper: (data) => data?.data?.products || [],
+    defaultValue: [],
+    executeOnMount: true,
+    execute: async (signal) => {
+      return axios.get("https://dummyjson.com/products?delay=4000", { signal });
+    },
+    promiseExecutionStatusHandlers: {
+      isRequestAbortionComplete: () => {
+        console.log("Aborting complete");
       },
-      retries: 3,
-      onretryQueueFailed: (err) => {
-        console.log('we failed to retry the queue', err);
-      },
-      onSuccess: (data) => {
-        console.log('Success', data);
-      },
-    }),
-    []
-  );
+    },
+  });
+  const { data } = currentState;
 
   useEffect(() => {
-    callApi();
+    setTimeout(() => {
+      cancelRequest();
+    }, 1000);
   }, []);
 
-  return <div>asdad</div>;
+  console.log("currentStatecurrentState", currentState);
+
+  if (currentState?.isPending) {
+    return <div>This is loading....</div>;
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        marginTop: "10px",
+      }}
+    >
+      {data?.map(({ title, id }) => (
+        <div
+          style={{ background: "white", width: "100%", color: "black" }}
+          key={id}
+        >
+          {title}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/hooks/usePromise.js
+++ src/hooks/usePromise.js
@@ -1,69 +1,141 @@
-import { useReducer, useEffect } from 'react'
+import { useReducer, useEffect, useCallback } from "react";
 
-export const STATE_TYPE = {
-    DATA: 'DATA',
-    LOADING: 'LOADING',
-    ERROR: 'ERROR'
-}
-
-const updatehandler = (prevState, action) => {
-    const { type = '', payload = '' } = action || {};
-
-    switch (type) {
-        case STATE_TYPE.DATA: {
-            prevState.data = payload
-            break;
-        }
-        case STATE_TYPE.LOADING: {
-            prevState.isPending = payload;
-            break;
-
-        }
-        case STATE_TYPE.ERROR: {
-            prevState.error = payload;
-            break;
-
-        }
-        default: {
-            return prevState;
-        }
-    }
-    return prevState;
-}
-
-const usePromise = ({
-    defaultValue = null,
-    executeOnMount = false,
-    defaultError = null,
-    execute = async () => { }
-}) => {
-    const [currentState, updateState] = useReducer(updatehandler, {
-        data: defaultValue || null,
-        isPending: executeOnMount,
-        error: defaultError || ''
-    });
-
-    const callApi =useCallback
-
-    useEffect(() => {
-
-        if (executeOnMount) {
-
-        }
-
-    }, [executeOnMount]);
-
-
-
-
-    return {
-        currentState,
-        updateState,
-    }
-
-
+const STATE_TYPE = {
+  DATA: "DATA",
+  LOADING: "LOADING",
+  ERROR: "ERROR",
+  RESET: "RESET",
 };
 
+const updatehandler = (prevState, action) => {
+  const { type = "", payload = "" } = action || {};
 
+  const currentState = { ...prevState };
 
-export { usePromise, STATE_TYPE }
+  switch (type) {
+    case STATE_TYPE.DATA: {
+      currentState.data = payload;
+      break;
+    }
+    case STATE_TYPE.LOADING: {
+      currentState.isPending = payload;
+      break;
+    }
+    case STATE_TYPE.ERROR: {
+      currentState.error = payload;
+      break;
+    }
+    case STATE_TYPE.RESET: {
+      return {
+        ...payload,
+      };
+    }
+    default: {
+      return prevState;
+    }
+  }
+  return currentState;
+};
+
+const isAbortErrorType = "CanceledError";
+
+const usePromise = ({
+  defaultValue = null,
+  executeOnMount = false,
+  defaultError = null,
+  execute = async () => {}, // expected values from the,
+  abortReasonDefault = "this request was cancelled",
+  promiseExecutionStatusHandlers = {
+    onSuccess: () => {},
+    onError: () => {},
+    onComplete: () => {},
+    isRequestAbortionComplete: () => {},
+  },
+  dataMapper = (data) => data,
+}) => {
+  const [currentState, updateState] = useReducer(updatehandler, {
+    data: defaultValue || null,
+    isPending: executeOnMount,
+    error: defaultError || "",
+    abortController: new AbortController(),
+  });
+
+  const cancelRequest = useCallback(
+    (reason = "") => {
+      currentState.abortController.abort(reason ?? abortReasonDefault);
+    },
+    [currentState.abortController, abortReasonDefault],
+  );
+
+  const resetState = useCallback(() => {
+    updateState({
+      type: STATE_TYPE.RESET,
+      payload: {
+        data: defaultValue || null,
+        isPending: executeOnMount,
+        error: defaultError || "",
+        abortController: new AbortController(),
+      },
+    });
+  }, [defaultValue, executeOnMount, defaultError]);
+
+  const callApi = useCallback(
+    async (...args) => {
+      if (typeof execute === "function") {
+        try {
+          updateState({
+            type: STATE_TYPE.LOADING,
+            payload: true,
+          });
+
+          const signal = currentState.abortController.signal;
+
+          const data = await execute(signal, ...args);
+
+          updateState({
+            type: STATE_TYPE.DATA,
+            payload: dataMapper(data),
+          });
+
+          promiseExecutionStatusHandlers?.onSuccess(data);
+        } catch (err) {
+          console.log("errerr", err?.name);
+          updateState({
+            type: STATE_TYPE.ERROR,
+            payload: err,
+          });
+          if (err.name === isAbortErrorType) {
+            promiseExecutionStatusHandlers?.isRequestAbortionComplete(err);
+          } else {
+            promiseExecutionStatusHandlers?.onError(err);
+          }
+        } finally {
+          updateState({
+            type: STATE_TYPE.LOADING,
+            payload: false,
+          });
+          promiseExecutionStatusHandlers?.onComplete();
+        }
+      }
+      return null;
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    },
+    [defaultValue, executeOnMount],
+  );
+
+  useEffect(() => {
+    if (executeOnMount) {
+      callApi();
+    }
+  }, [executeOnMount]);
+
+  return {
+    currentState,
+    updateState,
+    cancelRequest,
+    callApi,
+    resetState,
+  };
+};
+
+export { usePromise, STATE_TYPE };
