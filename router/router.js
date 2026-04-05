940769fa8072e78066308fced1e42f4325a4aeec
shakambhar pujari <shakambhar6562@gmail.com>
2026-04-05 17:55:03 +0530

    retry hook page added

--- router/router.js
+++ router/router.js
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router";
 import App from "../src/App";
 import PromisePage from "../src/pages/promise";
+import RetryHookPage from "../src/pages/retryHook";
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: App,
+    Component: RetryHookPage,
   },
   {
     path: "/usepromise",
---
+++ src/pages/retryHook.jsx
@@ -0,0 +1,33 @@
+import { useCallback, useEffect } from "react";
+import { withRetry } from "../utility/withRetry";
+
+function RetryHookPage() {
+  const callApi = useCallback(
+    // eslint-disable-next-line react-hooks/use-memo
+    withRetry({
+      callBackFn: (signal) => {
+        return defaultClient.GET({
+          mockEndpoint: defaultClient.availableEnpoints.productList,
+          sleepTimer: 2000,
+          signal,
+        });
+      },
+      retries: 3,
+      onretryQueueFailed: (err) => {
+        console.log("we failed to retry the queue", err);
+      },
+      onSuccess: (data) => {
+        console.log("Success", data);
+      },
+    }),
+    [],
+  );
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
+  return <div></div>;
+}
+
+export default RetryHookPage;
